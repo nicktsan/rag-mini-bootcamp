d320209fc355bdd429aa85f8061e248afc054d8a
Orvin Bellamy <orvinbellamy101@gmail.com>
2024-06-23 23:57:04 +0000

    Work update

--- workshop_code/common_components/vectorizers.py
+++ workshop_code/common_components/vectorizers.py
@@ -1,20 +1,32 @@
 from openai import OpenAI
-
+import os
 from typing import List
 
-OPENAI_CLIENT = OpenAI()
+# This is temporary, remove later
+OPENAI_CLIENT = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 EMBEDDING_MODEL = "text-embedding-3-small"
 
 class Vectorizer:
+
+    # This is for contents/texts, hence why need text splits
+    # Looks similar with vectorize_query, only difference is that one is for query only
+    # no text splits
     def vectorize_text_splits(self, text_splits: List[str]) -> List[List[float]]:
-        raise NotImplementedError("Implement this method and delete this exception.")
-        # response = ???  # TODO: write the API call to the OpenAI Embeddings API
+        
+        # model can be anything available
+        model = 'text-embedding-3-small'
+        response = OPENAI_CLIENT.embeddings.create(input=text_splits, model=model)
+        
         embeddings = []
-        # TODO: build the embeddings list from OpenAI's HTTP response
+        
+        # Each text split is an item hence why we have to loop this
+        for i in range(0, len(response.data)):
+            embeddings.append(response.data[i].embedding)
         return embeddings
     
     def vectorize_query(self, query: str) -> List[float]:
-        raise NotImplementedError("Implement this method and delete this exception.")
-        # response = ???  # TODO: write the API call to the OpenAI Embeddings API
+        
+        model = 'text-embedding-3-small'
+        response = OPENAI_CLIENT.embeddings.create(input=query, model=model)
         query_vector = response.data[0].embedding
         return query_vector
--- workshop_code/retrievers.py
+++ workshop_code/retrievers.py
@@ -8,7 +8,12 @@ class NaiveRetriever:
     self._vectorizer = vectorizer
     
   def retrieve(self, query: str) -> str:
+    
+    # Vectorize the query we are inputting
     query_vector = self._vectorizer.vectorize_query(query)
+
+    # retrieve here is retrieving data from WCS.
+    # Need to understand retrieve better
     retrieved_chunks_list = self._wcs_client_adapter.retrieve(query_vector, k=5)
     formatted_chunks = "\n\n".join(chunk for chunk in retrieved_chunks_list)
     return formatted_chunks
